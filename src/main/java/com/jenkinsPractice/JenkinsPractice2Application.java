c9ef6675513ea3991b324848aeb98e94dbf46d64
gems grab test <vaibhavpanwalkar39@gmail.com>
2024-10-15 12:16:40 +0530

    project deploy into git

---
+++ src/main/java/com/jenkinsPractice/JenkinsPractice2Application.java
@@ -0,0 +1,13 @@
+package com.jenkinsPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPractice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsPractice/JenkinsPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
